3020b8b3d81e956579531932b6dd5c6452ca9e2b
zulfadhiaulhaq <zdhiaulhaq20@gmail.com>
2020-07-09 13:03:34 +0700

    give env

--- src/app.js
+++ src/app.js
@@ -1,15 +1,35 @@
+require('dotenv').config()
 const express = require('express')
 const cors = require('cors')
-
 const api = require('./api')
 const not_found = require('./middlewares')
 
 const app = express()
-app.use(cors())
+
+const whitelist = [process.env.SERVER_URL, process.env.WEB_URL, process.env.SERVER_DEV, process.env.WEB_DEV]
+const corsOptions = {
+  origin: function (origin, callback) {
+    if (whitelist.indexOf(origin) !== -1 || !origin) {
+      callback(null, true)
+    } else {
+      const error = new Error(`Not allowed by CORS`, 'Error')
+      callback(error)
+    }
+  }
+}
+
+app.use(cors(corsOptions))
+
+app.use(function (req, res, next) {
+  res.removeHeader("X-Powered-By");
+  next();
+});
 
 app.get('/', (req, res) => {
     res.json({
         message: "Hello.... Welcome to Nanime Scraper API",
+        disclaimer_1: "This is just for fun and for my portofolio",
+        disclaimer_2: "and I'm not use this to commercial",
         how_to_use : {
             search: "/api/search/[anime_name]",
             detail_anime: "/api/animes/[type]/[link_name]",
@@ -27,7 +47,7 @@ app.get('/', (req, res) => {
                 see_detail: "/api/animes/anime/naruto",
                 see_video: "/api/watch/one-piece-episode-912",
             },
-            for_movie: {
+            "for_movie": {
                 see_detail_and_video: "/api/animes/movie/one-piece-movie-14-stampede",
             }
         }
